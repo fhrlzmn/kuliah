8098b8419bd0f122fb063c2f341efd3c1265e5d2
fhrlzmn <fhrlzmn26@gmail.com>
2023-06-19 15:42:37 +0700

    PHP Login: Styling standar

--- Semester 6/Pemrograman Internet/Tugas PHP Login/login.php
+++ Semester 6/Pemrograman Internet/Tugas PHP Login/login.php
@@ -1,31 +1,22 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-  <form action="/auth/handle_login.php" method="post">
-    <div>
-      <label for="username">Username : </label>
+  <main>
+    <form class="login" action="/auth/handle_login.php" method="post">
+      <h2>LOGIN</h2>
+      <label for="username">Username</label>
       <input type="text" name="username" id="username" required>
-    </div>
-
-    <div>
-      <label for="password">Password : </label>
+      <label for="password">Password</label>
       <input type="password" name="password" id="password" required>
-    </div>
-
-    <div>
       <input type="submit" value="Log In" name="login">
-      <a href="/register.php">Create a new account</a>
-    </div>
-  </form>
+      <p>Don't have an account? <a href="/register.php">Sign Up</a>
+    </form>
+  </main>
 </body>
 </html>
--- Semester 6/Pemrograman Internet/Tugas PHP Login/register.php
+++ Semester 6/Pemrograman Internet/Tugas PHP Login/register.php
@@ -1,53 +1,35 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Register</title>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-  <form action="/auth/handle_register.php" method="post">
-    <div>
-      <label for="name">Name : </label>
-      <input type="text" name="name" id="name" required>
-    </div>
+  <main>
+    <form class="register" action="/auth/handle_register.php" method="post">
+      <h2>REGISTER</h2>
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" placeholder="Your name" required>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" placeholder="Your email"required>
+          <label for="address">Address</label>
+          <input type="text" name="address" placeholder="Kp. example RT99" id="address">
 
-    <div>
-      <label for="email">Email : </label>
-      <input type="email" name="email" id="email" required>
-    </div>
+          <div class="gender">
+            <input type="radio" name="gender" id="male" value="L">
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="P">
+            <label for="female">Female</label>
+          </div>
 
-
-    <div>
-      <label for="address">Address : </label>
-      <input type="text" name="address" id="address">
-    </div>
-
-    <div>
-      <label for="gender">Gender : </label>
-      <input type="radio" name="gender" id="male" value="L">
-      <label for="male">Male</label>
-      <input type="radio" name="gender" id="female" value="P">
-      <label for="female">Female</label>
-    </div>
-
-    <div>
-      <label for="username">Username : </label>
-      <input type="text" name="username" id="username" required>
-    </div>
-
-    <div>
-      <label for="password">Password : </label>
-      <input type="password" name="password" id="password" required>
-    </div>
-
-    <div>
-      <input type="submit" value="Register" name="register">
-    </div>
-  </form>
+          <label for="username">Username</label>
+          <input type="text" name="username" id="username" placeholder="Username" required>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="Password" required>
+          <input type="submit" value="Register" name="register">
+    </form>
+  </main>
 </body>
 </html>
